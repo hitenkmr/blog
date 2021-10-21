3005e3478100377f90d0b4b98d6e5c3c03868c7f
Hitender Kumar <hitender@backbase.com>
2021-10-21 16:02:42 +0530

    configure App.js

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+ const IndexScreen = () => {
+    return <Text>Index</Text>
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default IndexScreen;
