16c719d2658e424ab85d9b9a3b4b4e6964d6a891
Hitender Kumar <hitender@backbase.com>
2021-10-21 18:07:43 +0530

    move data with context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { add } from 'react-native-reanimated';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>;
+
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    function addBlogPosts() {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+    }
+
+    return <BlogContext.Provider value={ {data: blogPosts, addBlogPosts: addBlogPosts} }>{children}</BlogContext.Provider>;
 };
- 
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,8 +1,35 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import BlogContext from "../context/BlogContext";
 
- const IndexScreen = () => {
-    return <Text>Index</Text>
+const IndexScreen = ({ navigation }) => {
+
+    const value = useContext(BlogContext);
+
+    console.log("data");
+
+    return <View>
+
+        <Button title='Add Post' onPress={() => { value.addBlogPosts() }}></Button>
+
+        <FlatList
+            keyExtractor={(item, index) => item.title}
+            data={value.data}
+            renderItem={({ item, index }) => {
+                return (
+                    <View>
+                        <Text>
+                            {item.title}
+                        </Text>
+                    </View>
+                );
+            }}
+        />
+
+        <Button title='See Posts' onPress={() => { navigation.push('Index') }}></Button>
+
+    </View>
 };
 
 const styles = StyleSheet.create({
