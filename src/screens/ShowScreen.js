2d1a27429fd5c176a039a014a3167d234a23da68
Hitender Kumar <hitender@backbase.com>
2021-10-22 15:59:42 +0530

    EditScreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,20 +1,31 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
- 
+import { EvilIcons } from '@expo/vector-icons';
+
 const ShowScreen = ({ navigation }) => {
     const { state, addBlogPost, deleteBlogPost, updateBlogPost } = useContext(Context);
     const id = navigation.getParam('id')
 
     function currentBlog() {
-       return state.find(data => data.id === id)
+        return state.find(data => data.id === id)
     }
 
     return (
         <View>
-            <Text>{ currentBlog().title }</Text>
+            <Text>{currentBlog().title}</Text>
         </View>
     );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+                <EvilIcons name="pencil" size={35} />
+            </TouchableOpacity>
+        ),
+    };
 };
 
 const styles = StyleSheet.create({
@@ -22,9 +33,9 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        borderBottomWidth:1,
-        borderTopWidth:1,
-        borderColor:'gray'
+        borderBottomWidth: 1,
+        borderTopWidth: 1,
+        borderColor: 'gray'
     },
 
     contentContainer: {
