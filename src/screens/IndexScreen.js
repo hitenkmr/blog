7130acb68e26b916d283f8c1c064a99e6b456cc0
Hitender Kumar <hitender@backbase.com>
2021-10-22 12:46:51 +0530

    style list row and add delete icon with touch

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const IndexScreen = () => {
     const { state, addBlogPost } = useContext(Context);
@@ -10,16 +12,41 @@ const IndexScreen = () => {
             <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost} />
             <FlatList
+                ItemSeparatorComponent={(props) => {
+                    return (<View style={styles.rowSeperator} />);
+                }}
+                contentContainerStyle={styles.contentContainer}
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>;
+                    return (<View style={styles.container}>
+                        <Text>{item.title}</Text>
+                        <TouchableOpacity onPress={() => {}}>
+                            <AntDesign name="delete" size={24} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                    );
                 }}
             />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+
+    contentContainer: {
+        paddingHorizontal: 20
+    },
+
+    rowSeperator: {
+        height: 10,
+        backgroundColor: 'lightgray'
+    }
+});
 
 export default IndexScreen;
